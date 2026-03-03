7385b25ad370500259b92c718aad678b6b751c92
Tubatse Katlego Moloi <tubatse.kmoloi@gmail.com>
2026-03-03 13:59:40 +0200

    added nav bar without contents and bentro grid section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,21 @@
-import HeroSection from "@/components/HeroSection";
+"use client";
+
+import HeroSection from "@/components/hero-section";
+import { FloatingNav } from "@/components/ui/floating-navbar";
+import { FaHome } from "react-icons/fa";
+import { Grid } from "@/components/grid"
+
 const Home = () => {
   return (
     <main className="relative bg-black/96 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={[
+        {name: 'Home', link: '/', icon: <FaHome/>}
+      ]}/>
+
       <HeroSection />
-      <div className="max-w-7xl w-full"></div>
+      <Grid/>
+      </div>
     </main>
   );
 };
